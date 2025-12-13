d01b28ca9333efada560c91388adb265a5757073
MuchhammadRevaldy <warunggazza@gmail.com>
2025-12-13 16:42:01 +0700

    huggingface

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,5 @@ php artisan view:cache
 php artisan migrate --force
 
 # Start Server
-# Using artisan serve is easiest for simple containers without Nginx config, 
-# though php-fpm is better for scaling. For Render/Fly Free tier, artisan serve is often sufficient for demos.
-php artisan serve --host=0.0.0.0 --port=8080
+# Hugging Face Spaces defaults to port 7860.
+php artisan serve --host=0.0.0.0 --port=${PORT:-7860}
